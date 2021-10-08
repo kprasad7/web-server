19be0682a33c5e6c46212564b0e4a66ef3a5a330
kprasad7 <kanuridurgaprasad049@gmail.com>
2021-10-08 18:49:51 +0530

    bug rectify

--- Taskmate/settings.py
+++ Taskmate/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
